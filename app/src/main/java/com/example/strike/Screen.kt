589f5a57c4869fb7ac44bf45b917374242c4c429
k-sameer701 <sameer2001kr@gmail.com>
2024-10-29 10:48:41 +0530

    Buttons Fixed

--- app/src/main/java/com/example/strike/Screen.kt
+++ app/src/main/java/com/example/strike/Screen.kt
@@ -6,17 +6,35 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.strike.ui.theme.StrikeTheme
+
+//@Preview
+//@Composable
+//fun Show(){
+//    StrikeTheme {
+//        Surface(
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            val viewModel = TicTacToeViewModel()
+//            TicTacToeScreen(viewModel = viewModel)
+//        }
+//    }
+//}
 
 @Composable
 fun TicTacToeScreen(
@@ -30,7 +48,7 @@ fun TicTacToeScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val turn = if(state.isXTurn) "X's Turn" else "O's Turn"
-        val turnMessage = "Tic Tac Toe\nIt is $turn"
+        val turnMessage = "Strike \nIt is $turn"
         val winner = state.winner
         val winnerMessage = "Tic Tac Toe\n$winner Wins"
         Text(
@@ -47,6 +65,7 @@ fun TicTacToeScreen(
         BuildRow(rowId = 3, viewModel = viewModel)
 
         Button(
+            shape = RoundedCornerShape(5.dp),
             onClick = {viewModel.resetBoard()},
             colors = ButtonDefaults.buttonColors(
                 containerColor = MaterialTheme.colorScheme.primary,
@@ -91,6 +110,8 @@ fun TicTacToeButton(
     val color = if(shouldChangeColor) MaterialTheme.colorScheme.tertiary
     else MaterialTheme.colorScheme.primary
     Button(
+        modifier = Modifier.size(85.dp),
+        shape = RoundedCornerShape(5.dp),
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(
             containerColor = color,
